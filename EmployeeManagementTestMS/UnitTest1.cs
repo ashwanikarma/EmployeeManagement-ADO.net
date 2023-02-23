a6ce30adac21db50ef6432f196d16c1f6c4aae39
ashwanikarma <ashwanikarma9@gmail.com>
2023-02-23 14:38:12 +0530

    [Ashwani]- added Employee management problem ADO.net

---
+++ EmployeeManagementTestMS/UnitTest1.cs
@@ -0,0 +1,29 @@
+using EmployeeManagement.Model.SalaryModel;
+using EmployeeManagement;
+
+namespace EmployeeManagementTestMS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                salaryId = 1,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 2
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+
+
+}
